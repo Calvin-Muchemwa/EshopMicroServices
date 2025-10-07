5e2192f72b450c6223148c2808887cfacbf1515d
Calvin Muchemwa <calvin.muchemwa@derivco.co.za>
2025-10-07 07:31:02 +0200

    Made some updates

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,5 @@
-using MediatR;
-
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(
@@ -7,14 +7,27 @@ public record CreateProductCommand(
     List<string> Category,
     string Description,
     string ImageFile,
-    decimal Price) :IRequest<CreateProductResult>;
+    decimal Price) :ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler :IRequestHandler<CreateProductCommand,CreateProductResult>
+public class CreateProductCommandHandler:ICommandHandler<CreateProductCommand,CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand createProductCommand,CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand createProductCommand,CancellationToken cancellationToken)
     {
-        
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var product = new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = createProductCommand.Name,
+            Category = createProductCommand.Category,
+            Description = createProductCommand.Description,
+            ImageFile = createProductCommand.ImageFile,
+            Price = createProductCommand.Price,
+        };
+
+        // Some business Logic
+        return await Task.Run(()=> new CreateProductResult(Guid.NewGuid()));
     }
 }
